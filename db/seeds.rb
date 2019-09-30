004d20b82a03ae63ac182e34cf60790b5c7d409b
yukijina <yukijina@gmail.com>
2019-09-30 06:32:12 -0700

    create user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+yuki = User.create(username: "yuki", password: "test")
