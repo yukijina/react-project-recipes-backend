1300b81456bdd15671e9af52b5eee6d65518daff
yukijina <yukijina@gmail.com>
2019-10-01 20:57:35 -0700

    add routes for login logout get_current_user

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   post "/api/v1/login", to: "api/v1/sessions#create"
   get "/api/v1/get_current_user", to: "api/v1/sessions#get_current_user"
+  delete "/api/v1/logout", to: "api/v1/sessions#destroy"
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
