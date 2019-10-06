ad74f430f97be25c3c01891f5350c8afe4854b9f
yukijina <yukijina@gmail.com>
2019-10-06 16:10:27 -0700

    create helper and find exsiting favorite (user-recipe)

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,4 +1,10 @@
 class Favorite < ApplicationRecord
     belongs_to :user
     belongs_to :recipe
+
+    def self.find_favorite(params)
+        where("user_id = ?", params[:favorite][:user_id]).first_or_initialize do |favorite|
+            favorite.user_id = params[:favorite][:user_id]
+        end
+    end 
 end
