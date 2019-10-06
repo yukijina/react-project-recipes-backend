9995f22a08e65ceeaede6ff406a40d2eca37b137
yukijina <yukijina@gmail.com>
2019-10-06 16:11:01 -0700

    add api_id attribute

--- app/serializers/recipe_serializer.rb
+++ app/serializers/recipe_serializer.rb
@@ -1,4 +1,4 @@
 class RecipeSerializer < ActiveModel::Serializer
-  attributes :id, :title, :image
+  attributes :id, :title, :image, :api_id
   has_many :favorites, serializer: RecipeFavoriteSerializer
 end
