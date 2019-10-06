d1e7e74a37115a9e3a15d4dca4428afcf88869df
yukijina <yukijina@gmail.com>
2019-10-06 11:50:42 -0700

    add favorite routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,9 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users
-    end
+      resources :recipes
+      resources :favorites
+        end
   end
 
 end
