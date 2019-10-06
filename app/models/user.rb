ccbb5b0e036b01b195611087f524550816d962ed
yukijina <yukijina@gmail.com>
2019-10-06 11:54:27 -0700

    add has_many relationship

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+    has_many :favorites
+    has_many :recipes, through: :favorites
     has_secure_password
     validates :username, presence: true, uniqueness: true
 end
