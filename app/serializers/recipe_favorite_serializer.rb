a9b861a232624025a1233033e50189b64406e81b
yukijina <yukijina@gmail.com>
2019-10-13 01:28:31 -0700

    add user_name attribute

--- app/serializers/recipe_favorite_serializer.rb
+++ app/serializers/recipe_favorite_serializer.rb
@@ -1,3 +1,3 @@
 class RecipeFavoriteSerializer < ActiveModel::Serializer
-  attributes :id, :like, :review, :user_id, :recipe_id
+  attributes :id, :like, :review, :user_id, :recipe_id, :user_name
 end
